12007e410cd737570c60df31aff8920bf6a973a3
Anna Miklukha <miklukha.anna@ukr.net>
2022-05-26 21:42:42 +0200

    start write styles for task 1

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,26 +4,28 @@ import s from './Profile.module.css';
 export function Profile({ username, tag, location, avatar, stats }) {
   return (
     <>
-      <div className="profile">
-        <div className="description">
-          <img src={avatar} alt={username} className="avatar" />
-          <p className="name">{username}</p>
-          <p className="tag">@{tag}</p>
-          <p className="location">{location}</p>
+      <div className={s.profile}>
+        <div className={s.description}>
+          <div className={s.avatar}>
+            <img src={avatar} alt={username} />
+          </div>
+          <p className={s.name}>{username}</p>
+          <p className={s.tag}>@{tag}</p>
+          <p className={s.location}>{location}</p>
         </div>
 
-        <ul className="stats">
-          <li>
-            <span className="label">Followers</span>
-            <span className="quantity">{stats.followers}</span>
+        <ul className={s.stats}>
+          <li className={s.item}>
+            <span className={s.label}>Followers</span>
+            <span className={s.quantity}>{stats.followers}</span>
           </li>
-          <li>
-            <span className="label">Views</span>
-            <span className="quantity">{stats.views}</span>
+          <li className={s.item}>
+            <span className={s.label}>Views</span>
+            <span className={s.quantity}>{stats.views}</span>
           </li>
-          <li>
-            <span className="label">Likes</span>
-            <span className="quantity">{stats.likes}</span>
+          <li className={s.item}>
+            <span className={s.label}>Likes</span>
+            <span className={s.quantity}>{stats.likes}</span>
           </li>
         </ul>
       </div>
